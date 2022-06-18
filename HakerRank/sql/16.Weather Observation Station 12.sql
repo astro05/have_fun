c2f4ad6bfa0f32953be5629155981f3730137cf5
astro05 <42905945+astro05@users.noreply.github.com>
2022-06-18 23:47:08 +0600

    Create 16.Weather Observation Station 12.sql

---
+++ HakerRank/sql/16.Weather Observation Station 12.sql
@@ -0,0 +1,24 @@
+/*
+https://www.hackerrank.com/challenges/weather-observation-station-12/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+*/
+
+/*
+select distinct city from station
+where not(
+    substring(city,len(city),len(city)) = 'a' or 
+substring(city,len(city),len(city)) = 'e' or 
+substring(city,len(city),len(city)) = 'i' or
+substring(city,len(city),len(city)) = 'o' or 
+substring(city,len(city),len(city)) = 'u'
+)and not  (
+     substring(city,1,1) = 'a' or substring(city,1,1) = 'e' or substring(city,1,1) = 'i' or substring(city,1,1) = 'o' or substring(city,1,1) = 'u'
+);
+*/
+
+
+select distinct city from station
+where not (
+right(city,1) = 'a' or right(city,1) = 'e' or right(city,1) = 'i' or right(city,1) = 'o' or right(city,1) = 'u'
+) and not(
+left(city,1) = 'a' or left(city,1) = 'e' or left(city,1) = 'i' or left(city,1) = 'o' or left(city,1) = 'u'
+);
