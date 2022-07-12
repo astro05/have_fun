546aaeb4cde25f5961d3dbce99047e3984df0144
astro05 <42905945+astro05@users.noreply.github.com>
2022-07-12 23:29:48 +0600

    Create 1-1 Sum of Two Digits.cpp

---
+++ Coursera/Algorithmic Toolbox/1-1 Sum of Two Digits.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+  
+
+int main()
+{
+    int num1;
+    int num2;
+    int sum;
+    
+    cin>>num1;
+    cin>>num2;
+
+    sum = num1 + num2;
+
+
+    cout<<sum;
+
+    return 0;
+}
