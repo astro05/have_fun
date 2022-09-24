c97612fb57d1a01e5ba36314022374cde4a7b1db
astro05 <joyultimates@gmail.com>
2022-09-25 00:26:11 +0600

    #

--- test/test c#/test/Program.cs
+++ test/test c#/test/Program.cs
@@ -1,29 +1,48 @@
 ﻿using System;
+using System.Net.NetworkInformation;
 
 namespace test
 {
     class program
     {
+        public static ulong pisano(ulong m)
+        {
+            ulong prev = 0;
+            ulong curr = 1;
+            ulong res = 0;
+
+            for (ulong i = 0; i < m * m; i++)
+            {
+                ulong temp = 0;
+                temp = curr;
+                curr = (prev + curr) % m;
+                prev = temp;
+
+                if (prev == 0 && curr == 1)
+                    res = i + 1;
+            }
+            return res;
+        }
         public static ulong fibonacci_lastdigit(ulong n, ulong m)
         {
-
+            ulong pisanoPeriod = pisano(m);
+            n = n % pisanoPeriod;
 
             if (n <= 1)
                 return (ulong)n;
 
             ulong previous = 0;
             ulong current = 1;
-            ulong temp_current = 0;
 
             for (ulong i = 0; i < n - 1; ++i)
             {
-                ulong tmp_previous = previous % m;
-                previous = current % m;
-                current = tmp_previous + current;
-                temp_current = current % m;
+                ulong tmp_previous = current;
+                current = (previous + current) % m;
+                previous = tmp_previous;
+
             }
 
-            return (ulong)(temp_current);
+            return (ulong)(current);
         }
 
         static void Main(string[] args)
@@ -35,9 +54,7 @@ namespace test
             n = (ulong)Convert.ToInt64(tokens[0]);
             m = (ulong)Convert.ToInt64(tokens[1]);
 
-            temp = n % (m * m - 1);
-            Console.WriteLine(temp + " " + m);
-            System.Console.WriteLine(fibonacci_lastdigit(temp,m));
+            System.Console.WriteLine(fibonacci_lastdigit(n,m));
             
 
         }
