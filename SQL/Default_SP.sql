891712077a6c6848e6b266314329e3dd872956b2
astro05 <42905945+astro05@users.noreply.github.com>
2023-01-25 15:02:33 +0600

    Update Default_SP.sql

--- SQL/Default_SP.sql
+++ SQL/Default_SP.sql
@@ -1,7 +1,26 @@
---to get columns name with data type use this
+/*Finding the data types of a SQL temporary table*/
+
+--to get columns name with data type - Using SP_HELP
 EXEC tempdb.dbo.sp_help N'#temp_table_name';
 
---to get specific columns name with data type use this
+--to get columns name with data type - Using SP_COLUMNS
+EXEC TempDB..SP_COLUMNS '#temp_table_name';
+
+--to get specific columns name with data type - Using SP_COLUMNS
 SELECT * 
 FROM tempdb.sys.columns 
-WHERE [object_id] = OBJECT_ID(N'tempdb..#temp');
+WHERE [object_id] = OBJECT_ID(N'tempdb..#temp_table_name');
+
+-- Using System Tables like INFORMATION_SCHEMA.COLUMNS, SYS.COLUMNS, SYS.TABLES
+SELECT * FROM TempDB.INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME IN (
+SELECT NAME FROM TempDB.SYS.TABLES WHERE OBJECT_ID=OBJECT_ID('TempDB.dbo.#TempTable')
+);
+GO
+
+SELECT * FROM TempDB.SYS.COLUMNS WHERE OBJECT_ID=OBJECT_ID('TempDB.dbo.#TempTable');
+GO
+
+SELECT * FROM TempDB.SYS.TABLES WHERE OBJECT_ID=OBJECT_ID('TempDB.dbo.#TempTable');
+GO
+
+/* ------------------- */
