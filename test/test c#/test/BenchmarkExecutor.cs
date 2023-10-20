e38444709b00e662e4474d6f45cf35236ab12992
astro5_L <joyultimates@gmail.com>
2023-10-21 01:28:00 +0600

    benchmark

---
+++ test/test c#/test/BenchmarkExecutor.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    [ShortRunJob]
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class BenchmarkExecutor
+    {
+        [Benchmark(Baseline = true)]
+       public void BenchMarkExecutor()
+        {
+            char[] c = { 'a', 'b', 'c', 'd', 'e' };
+            program.TwoSum(c);
+        }
+        
+        [Benchmark]
+        public void BenchMarkExecutor2()
+        {
+            char[] c = { 'a', 'b', 'c', 'd', 'e' };
+            program.TwoSum(c);
+        }
+        
+        [Benchmark]
+        public void BenchMarkExecutor3()
+        {
+            char[] c = { 'a', 'b', 'c', 'd', 'e' };
+            program.TwoSum(c);
+        }
+
+
+    }
+
+
+}
--- test/test c#/test/Program.cs
+++ test/test c#/test/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
+using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
@@ -10,45 +11,22 @@ namespace test
 
         static void Main(string[] args)
         {
-            int[] denominations = { 1, 4 , 6 };
-            int money = 8;
-            Change(money, denominations);
-
+            BenchmarkRunner.Run<BenchmarkExecutor>();
 
         }
 
-        static int Change(int money, int[] denominations)
+        public static void TwoSum(char[] s)
         {
-            int numCoins = 0;
-            int maxCoin = 0;
-            string changeMoney = "Change money is : ";
-            while (money > 0)
+            for (int i = 1; i <= s.Length/2; i++)
             {
-                maxCoin = denominations.Max();
-                if (money >= maxCoin)
-                {
-                    money = money - maxCoin;
-                    numCoins++;
-
-                    changeMoney = changeMoney + maxCoin + " + ";
-                }
-                else
-                {
-                    denominations = denominations.Where(val => val != maxCoin).ToArray();
-                }
-
-
-
+                (s[i-1], s[^i]) = (s[^i], s[i-1]);
             }
-            Console.WriteLine("Number of coins is: " + numCoins);
-            Console.WriteLine(changeMoney);
-
-            return 0;
         }
 
+        public  void te()
+        {
 
-
-
+        }
     }
 }
 
