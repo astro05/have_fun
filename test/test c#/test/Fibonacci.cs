11134b39644fc7b8845e5dbdbef9a2974eee0469
joy <joyultimates@gmail.com>
2024-02-25 18:10:16 +0600

    fibonacci memorization

---
+++ test/test c#/test/Fibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class Fibonacci
+    {
+        public static int FibonacciRecursiveMemoized(int n, Dictionary<int, int> memo)
+        {
+            if (memo.ContainsKey(n))
+            {
+                return memo[n];
+            }
+
+            if (n <= 1)
+            {
+                memo[n] = n;
+                return n;
+            }
+
+            int fib = FibonacciRecursiveMemoized(n - 1, memo) + FibonacciRecursiveMemoized(n - 2, memo);
+            memo[n] = fib;
+            return fib;
+        }
+
+    }
+}
--- test/test c#/test/Program.cs
+++ test/test c#/test/Program.cs
@@ -1,19 +1,67 @@
-﻿using BenchmarkDotNet.Running;
-using Microsoft.Diagnostics.Tracing.Parsers.IIS_Trace;
-using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-
-namespace test
+﻿using System;
+namespace OCPDemo
 {
-    class program
+    //Create an interface for the discount strategy
+    public interface IDiscountStrategy
     {
-        static void Main(string[] args)
+        double CalculateDiscount(double price);
+    }
+
+    //Implement this interface for each discount type
+    public class RegularDiscount : IDiscountStrategy
+    {
+        public double CalculateDiscount(double price)
         {
-            // BenchmarkRunner.Run<BenchmarkExecutor>();
-           
-         }
+            return price * 0.1;
+        }
+    }
+
+    public class PremiumDiscount : IDiscountStrategy
+    {
+        public double CalculateDiscount(double price)
+        {
+            return price * 0.3;
+        }
+    }
+
+    public class NewbieDiscount : IDiscountStrategy
+    {
+        public double CalculateDiscount(double price)
+        {
+            return price * 0.05;
+        }
+    }
+
+    //Modify the DiscountCalculator class to accept an IDiscountStrategy
+    public class DiscountCalculator
+    {
+        private readonly IDiscountStrategy _discountStrategy;
+
+        public DiscountCalculator(IDiscountStrategy discountStrategy)
+        {
+            _discountStrategy = discountStrategy;
+        }
+
+        public double CalculateDiscount(double price)
+        {
+            return _discountStrategy.CalculateDiscount(price);
+        }
+    }
+
+    //Testing the Open-Closed Principle
+    public class Program
+    {
+        public static void Main()
+        {
+            var regularDiscount = new RegularDiscount();
+            var calculator = new DiscountCalculator(regularDiscount);
+            double discountedPrice = calculator.CalculateDiscount(100); // 10% discount applied
+
+            var premiumDiscount = new PremiumDiscount();
+            calculator = new DiscountCalculator(premiumDiscount);
+            discountedPrice = calculator.CalculateDiscount(100); // 30% discount applied
+
+            Console.ReadKey();
+        }
     }
 }
-
