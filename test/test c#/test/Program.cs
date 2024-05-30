b4a89351e00dd153eb59e0bbf7a1a92670be8fd4
joy <joyultimates@gmail.com>
2024-05-30 16:30:10 +0600

    factory

--- test/test c#/test/Program.cs
+++ test/test c#/test/Program.cs
@@ -10,22 +10,14 @@ namespace Preprocessor
 
         static  void Main(string[] args)
         {
-            Parallel.Invoke(
-                () =>
-                {
-                    EagerLoadingSingleton s1 = EagerLoadingSingleton.GetInstance();
-                    s1.print("first");
-                },
-                () =>
-                {
-                    EagerLoadingSingleton s2 = EagerLoadingSingleton.GetInstance();
-                    s2.print("second");
-                }
-                );
+            new Client().Main();
+          
 
             Console.ReadKey();
         }
 
     }
+
+
 
 }
